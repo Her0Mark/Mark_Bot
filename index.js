14018fda69672211e84a0d53c728a1d22b7af2a1
Hero_Mark <52718477+Her0Mark@users.noreply.github.com>
2019-10-21 13:52:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -148,7 +148,7 @@ String.prototype.replaceAll = function(search, replacement) {
 client.on('message', msg => {
 
 // Quits the Bot
-if (msg.content === '!quitbot' && msg.author.id === '109122837524017152') {
+if (msg.content === '!quitbot' && msg.author.id === '1319457567728205824') {
   msg.reply('Goodbye!');
   client.destroy();
   console.log('Disconnected...');
